40d68573abc09587e77f65bc6b3d6e3897e6be32
kinchiller <kingchiller@gmail.com>
2021-03-13 23:02:45 +0100

    test

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -5,9 +5,9 @@ const sitemap = require('nextjs-sitemap-generator')
 
 sitemap({
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  pagesDirectory: __dirname + `/out`,
+  pagesDirectory: __dirname + `/pages`,
   targetDirectory: `public/`,
-  ignoredExtensions: ['png', 'jpg', 'ico', 'json', 'js', 'txt', 'xml'],
+  ignoredExtensions: ['png', 'jpg'],
   ignoredPaths: ['l', 'api'],
 })
 console.log(`✅ sitemap.xml generated!`)
