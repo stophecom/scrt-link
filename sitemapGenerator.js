11fc81229781e90011ae7a60fc563a1819344c08
kinchiller <kingchiller@gmail.com>
2021-03-13 22:09:29 +0100

    Text sitemapGenerator for vercel

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -3,9 +3,14 @@ require('dotenv').config({ path: __dirname + '/.env.production' })
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const sitemap = require('nextjs-sitemap-generator')
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs')
+
+const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString()
+
 sitemap({
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  pagesDirectory: __dirname + `/.next/server/pages`,
+  pagesDirectory: __dirname + '/.next/server/static/' + BUILD_ID + '/pages',
   targetDirectory: `out/`,
   ignoredExtensions: ['png', 'jpg'],
   ignoredPaths: ['l', 'api'],
