0e15a0696d45aee7982895980a700942078669a3
kinchiller <kingchiller@gmail.com>
2021-03-13 21:56:39 +0100

    Custom error pages

---
+++ pages/500.js
@@ -0,0 +1,1 @@
+export { default } from '@/views/Error/500'
---
+++ src/views/Error/404.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+import BaseButton from '@/components/BaseButton'
+import Page from '@/components/Page'
+
+const Error = () => (
+  <Page title="Error 404" subtitle={`Sorry, something went wrong.`}>
+    <Box>
+      <BaseButton href="/" color="primary" variant="contained" size="large">
+        Home
+      </BaseButton>
+    </Box>
+  </Page>
+)
+
+export default Error
