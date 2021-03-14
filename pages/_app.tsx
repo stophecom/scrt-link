9bba8023251ad027d0d384682902ff8418f88a81
kinchiller <kingchiller@gmail.com>
2021-03-14 23:15:06 +0100

    Fix relative manifest link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -74,7 +74,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content={theme.palette.primary.main} />
         <meta
           name="keywords"
