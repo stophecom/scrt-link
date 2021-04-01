7d06c47bcdc69bdf207488f3859afc6aa0116c54
kinchiller <kingchiller@gmail.com>
2021-04-01 22:33:00 +0200

    console.log response

--- src/views/Alias/index.tsx
+++ src/views/Alias/index.tsx
@@ -274,6 +274,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   let error
   try {
     const response = await axios.get(`${process.env.baseUrl}/api?alias=${alias}`)
+    console.log(response)
     const { data } = response
     const { secretType, message, isEncryptedWithUserPassword } = data
 
