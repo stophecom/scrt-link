08f51b8ca1d6c13a5807b4baf73df05f529e0734
kinchiller <kingchiller@gmail.com>
2021-04-02 22:37:36 +0200

    Disable PWA for now

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
     clientsClaim: false,
+    register: false, // Disable for now
   },
   async headers() {
     return [
