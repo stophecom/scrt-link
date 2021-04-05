8777ee51929580f5e97247ce894d76c6aaf46c4b
kinchiller <kingchiller@gmail.com>
2021-04-05 10:27:42 +0200

    Update twitter handle

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,8 @@
 export const appTitle = 'scrt.link'
 export const urlAliasLength = 21
 export const maxMessageLength = 280
-export const twitterLink = 'https://twitter.com/stophecom'
-export const twitterHandle = '@stophecom'
+export const twitterLink = 'https://twitter.com/ScrtLink'
+export const twitterHandle = '@ScrtLink'
 export const email = 'pssst@scrt.link'
 export const emailSantihans = 'info@santihans.com'
 export const gitlab = 'https://gitlab.com/kingchiller'
-export const github = 'https://github.com/christopheschwyzer'
