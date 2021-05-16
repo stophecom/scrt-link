e729a3a6b9ad28829c0d82d5d4e1f6f8ffc30604
kinchiller <kingchiller@gmail.com>
2021-05-16 21:21:54 +0200

    Improve code

--- pages/api/plans/index.ts
+++ pages/api/plans/index.ts
@@ -1,36 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import stripe from '@/api/utils/stripe'
+import createError from '@/api/utils/createError'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const { data } = await stripe.products.list()
+  switch (req.method) {
+    case 'GET': {
+      try {
+        const { data } = await stripe.products.list()
 
-      const getPlans = async () =>
-        Promise.all(
-          data.map(async (item) => {
-            const { data } = await stripe.prices.list({
-              product: item.id,
-              active: true,
-            })
+        const getPlans = async () =>
+          Promise.all(
+            data.map(async (item) => {
+              const { data } = await stripe.prices.list({
+                product: item.id,
+                active: true,
+              })
 
-            const priceByInterval = (interval: string) =>
-              data.find(({ recurring }) => recurring?.interval === interval)
+              const priceByInterval = (interval: string) =>
+                data.find(({ recurring }) => recurring?.interval === interval)
 
-            return {
-              name: item.name,
-              prices: { monthly: priceByInterval('month'), yearly: priceByInterval('year') },
-            }
-          }),
-        )
+              return {
+                name: item.name,
+                id: item.id,
+                prices: { monthly: priceByInterval('month'), yearly: priceByInterval('year') },
+              }
+            }),
+          )
 
-      res.status(200).json(await getPlans())
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err.message })
+        res.status(200).json(await getPlans())
+      } catch (err) {
+        res.status(500).json({ statusCode: 500, message: err.message })
+      }
+      break
     }
-  } else {
-    res.setHeader('Allow', 'GET')
-    res.status(405).end('Method Not Allowed')
+    default:
+      throw createError(405, 'Method Not Allowed')
   }
 }
