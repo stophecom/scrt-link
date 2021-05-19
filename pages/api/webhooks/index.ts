6ff72f6ea00db720c5a0306e16138cacf67ed3a9
kinchiller <kingchiller@gmail.com>
2021-05-19 21:40:47 +0200

    Cleanup webhooks, fix types

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -1,9 +1,12 @@
 import { buffer } from 'micro'
-import Cors from 'micro-cors'
-import { NextApiRequest, NextApiResponse } from 'next'
-import Stripe from 'stripe'
 
+import { NextApiHandler } from 'next'
+import Stripe from 'stripe'
+import NextCors from 'nextjs-cors'
+
+import withDb from '@/api/middlewares/withDb'
 import stripe from '@/api/utils/stripe'
+import createError from '@/api/utils/createError'
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!
 
@@ -14,11 +17,19 @@ export const config = {
   },
 }
 
-const cors = Cors({
-  allowMethods: ['POST', 'HEAD'],
-})
+const handler: NextApiHandler = async (req, res) => {
+  // Run the middleware
+  await NextCors(req, res, {
+    methods: ['HEAD', 'POST'],
+    origin: '*',
+  })
 
-const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const models = req.models
+
+  if (!models) {
+    throw createError(500, 'Could not find db connection')
+  }
+
   if (req.method === 'POST') {
     const buf = await buffer(req)
     const sig = req.headers['stripe-signature']!
@@ -30,33 +41,57 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     } catch (err) {
       // On error, log and return the error message.
       console.log(`❌ Error message: ${err.message}`)
-      res.status(400).send(`Webhook Error: ${err.message}`)
-      return
+      throw createError(400, `Webhook Error: ${err.message}`)
     }
 
     // Successfully constructed event.
     console.log('✅ Success:', event.id)
 
-    // Cast event data to Stripe object.
-    if (event.type === 'payment_intent.succeeded') {
-      const paymentIntent = event.data.object as Stripe.PaymentIntent
-      console.log(`💰 PaymentIntent status: ${paymentIntent.status}`)
-    } else if (event.type === 'payment_intent.payment_failed') {
-      const paymentIntent = event.data.object as Stripe.PaymentIntent
-      console.log(`❌ Payment failed: ${paymentIntent.last_payment_error?.message}`)
-    } else if (event.type === 'charge.succeeded') {
-      const charge = event.data.object as Stripe.Charge
-      console.log(`💵 Charge id: ${charge.id}`)
-    } else {
-      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`)
+    switch (event.type) {
+      case 'checkout.session.completed': {
+        const paymentStatus = event.data.object as Stripe.Checkout.Session
+
+        await models.Customer.findOneAndUpdate(
+          {
+            'stripe.customerId': paymentStatus.customer,
+          },
+          { 'stripe.subscription': paymentStatus.subscription },
+        )
+
+        console.log(`💰 Session completed status: ${JSON.stringify(paymentStatus, null, 2)}`)
+        break
+      }
+
+      // Cast event data to Stripe object.
+      case 'payment_intent.succeeded': {
+        const paymentIntent = event.data.object as Stripe.PaymentIntent
+        console.log(`💰 PaymentIntent status: ${paymentIntent.status}`)
+        break
+      }
+
+      case 'payment_intent.payment_failed': {
+        const paymentIntent = event.data.object as Stripe.PaymentIntent
+        console.log(`❌ Payment failed: ${paymentIntent.last_payment_error?.message}`)
+        break
+      }
+
+      case 'charge.succeeded': {
+        const charge = event.data.object as Stripe.Charge
+
+        console.log(`💵 Charge id: ${charge.id}`)
+
+        break
+      }
+      default: {
+        console.warn(`Unhandled event type: ${event.type}`)
+      }
     }
 
     // Return a response to acknowledge receipt of the event.
     res.json({ received: true })
   } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method Not Allowed')
+    throw createError(405, 'Method Not Allowed')
   }
 }
 
-export default cors(webhookHandler as any)
+export default withDb(handler)
