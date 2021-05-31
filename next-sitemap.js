584fd5f5ace552c57e6f5f48a26c6d575e7aa01d
kinchiller <kingchiller@gmail.com>
2021-05-31 22:26:48 +0200

    Fix sitemap generator

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({ path: __dirname + '/.env.production' })
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://scrt.link',
+  generateRobotsTxt: false, // (optional)
+  exclude: ['/l/*', '/api/*', '/404', '/500', '/account'],
+}
