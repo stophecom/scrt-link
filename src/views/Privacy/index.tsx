184fbcd226f27f637ff4c0f8af08d1b7d40783b7
kinchiller <kingchiller@gmail.com>
2021-06-01 09:32:52 +0200

    Update privacy page

--- src/views/Privacy/index.tsx
+++ src/views/Privacy/index.tsx
@@ -6,13 +6,13 @@ import { BaseButtonLink } from '@/components/Link'
 import Page from '@/components/Page'
 
 const Privacy = () => (
-  <Page title="Privacy" subtitle={`No ads, no cookies*, no tracking, no bullshit.`}>
+  <Page title="Privacy" subtitle={`No ads, no tracking, no bullshit.`}>
     <Box mb={6}>
       <Typography variant="h3">Philosophy</Typography>
       <Typography variant="body1">
         <Markdown
           source={`
-This project is all about the respect for people's integrity and privacy. We have no interest in you! Not in you as a person, nor in your secrets. We don't care 😎 
+This project is all about the respect for people's integrity and privacy. We have no interest in you! Not in you as a person, nor in your secrets. 
 `}
         />
       </Typography>
@@ -36,14 +36,14 @@ We do basic web traffic analytics with [Plausible.io](https://plausible.io/) -  
     </Box>
 
     <Box mb={6}>
-      <Typography variant="h3">*Cookies</Typography>
+      <Typography variant="h3">{`Account & Cookies`}</Typography>
 
       <Typography variant="body1">
         <Markdown
           source={`
-For people visiting our website, we don't use cookies whatsoever. However, for people who create an account, the use of esssential cookies is necessary to handle authentication states.
+For power users, we offer user accounts that require authentification. Since privacy is a top concern, we only ask for the bare minimum. In fact, you can create an account with only an email address (You may use a temporary or masked email) - no further information is mandatory. 
 
-You can create an account with only an email address (You may use a temporary or masked email) - no further information is mandatory. Read more in our Privacy Policy.
+For people visiting our website, we only ever use essenital cookies. In other words, cookies that are necessary for providing core functionalities: E.g. managing secure authentication. Read more in our Privacy Policy.
 `}
         />
       </Typography>
