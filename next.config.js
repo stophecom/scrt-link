a5c4cfc5a7796b2fb59d301be03ab87420aeaaa2
kinchiller <kingchiller@gmail.com>
2021-06-09 21:35:33 +0200

    Revert "Remove cache settings"
    This reverts commit 96aebb115a849dc9142f87c69bd7b52e2d4a331f.

--- next.config.js
+++ next.config.js
@@ -11,6 +11,27 @@ module.exports = withPWA({
     clientsClaim: false,
     register: false, // Disable for now
   },
+  async headers() {
+    return [
+      {
+        source: '/l/:alias',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, max-age=0',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'DNT',
+            value: '1',
+          },
+        ],
+      },
+    ]
+  },
 
   webpack: (config) => {
     config.module.rules.push({
