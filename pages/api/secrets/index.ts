5a18b300c616f3326a133c70586a2b3c872cdccd
kinchiller <kingchiller@gmail.com>
2021-09-02 10:00:05 +0200

    Remove OPTIONS request

--- pages/api/secrets/index.ts
+++ pages/api/secrets/index.ts
@@ -24,11 +24,6 @@ const handler: NextApiHandler = async (req, res) => {
     origin: '*',
   })
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-
   const models = req.models
   if (!models) {
     throw createError(500, 'Could not find db connection')
