585133f1afb305d7b859bfb839e45cd39e7f66c8
kinchiller <kingchiller@gmail.com>
2021-10-02 15:28:21 +0200

    Add twilio SMS service

--- src/api/utils/mailjet.ts
+++ src/api/utils/mailjet.ts
@@ -19,16 +19,3 @@ const mailjet = ({ To, Subject = 'Shhh', ...props }: Partial<Email.SendParamsMes
     })
 
 export default mailjet
-
-export const mailjetSms = (props: Omit<SMS.SendParams, 'From'>) =>
-  mj
-    .connect(process.env.MJ_SMS_TOKEN, {
-      url: 'api.mailjet.com', // default is the API url
-      version: 'v4', // default is '/v3'
-      perform_api_call: true, // used for tests. default is true
-    })
-    .post('sms-send')
-    .request({
-      From: 'scrt.link',
-      ...props,
-    })
