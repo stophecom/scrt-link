1b1df8089cc636943a88d3cce9bfcf3e650134fd
kinchiller <kingchiller@gmail.com>
2021-10-14 22:41:20 +0200

    Add stripe customer portal link to account

---
+++ pages/api/subscriptions/create-portal-link/index.ts
@@ -0,0 +1,42 @@
+import { NextApiHandler } from 'next'
+import { getSession } from 'next-auth/client'
+
+import withDb from '@/api/middlewares/withDb'
+import handleErrors from '@/api/middlewares/handleErrors'
+import stripe from '@/api/utils/stripe'
+import createError from '@/api/utils/createError'
+import { getBaseURL } from '@/utils'
+
+const handler: NextApiHandler = async (req, res) => {
+  const models = req.models
+
+  const session = await getSession({ req })
+
+  if (!models) {
+    throw createError(500, 'Could not find db connection')
+  }
+  if (!session) {
+    throw createError(405, 'Not allowed. You need to be signed in!')
+  }
+
+  switch (req.method) {
+    case 'POST': {
+      try {
+        const { url } = await stripe.billingPortal.sessions.create({
+          customer: session.stripeCustomerId,
+          return_url: `${getBaseURL()}/account`,
+        })
+
+        res.status(200).json({ url })
+      } catch (err) {
+        throw createError(500, err instanceof Error ? err.message : 'Unexpected error')
+      }
+      break
+    }
+
+    default:
+      throw createError(405, 'Method Not Allowed')
+  }
+}
+
+export default handleErrors(withDb(handler))
