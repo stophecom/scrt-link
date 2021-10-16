8e68cac63637f3a57c25525ed64fb508706e41f3
kinchiller <kingchiller@gmail.com>
2021-10-16 12:46:26 +0200

    Add sentry env config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -10,6 +10,7 @@ Sentry.init({
   dsn: SENTRY_DSN || 'https://ac1aa77891a440fc9851ab72d428c925@o159456.ingest.sentry.io/5987206',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  environment: process.env.NEXT_PUBLIC_ENV,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
