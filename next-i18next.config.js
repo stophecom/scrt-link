c3933738c6e56b8ae825cb4a829717e29c444840
kinchiller <kingchiller@gmail.com>
2021-12-08 22:40:50 +0100

    Remove custom locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,6 @@
-const path = require('path')
-
-const isProduction = process.env.NEXT_PUBLIC_ENV === 'production'
-
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
-  ...(isProduction ? { localePath: path.resolve('./public/locales') } : {}), // Needs to be set for production
 }
