24cc7021aa8ca7137440be9bf2726a26cb75dd11
kinchiller <kingchiller@gmail.com>
2022-02-01 20:35:48 +0100

    Add helper function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -48,3 +48,12 @@ export const abbrNum = (number: number, decPlaces: number) => {
 
   return abbrNum
 }
+
+export const formatBytes = (bytes: number, decimals?: number) => {
+  if (bytes == 0) return '0 Bytes'
+  let k = 1024,
+    dm = decimals || 2,
+    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+}
