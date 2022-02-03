cf386f662e486b7b8ddb86973936972c8abc1d37
kinchiller <kingchiller@gmail.com>
2022-02-03 22:51:06 +0100

    Switch to new AWS SDK

--- src/api/utils/s3.ts
+++ src/api/utils/s3.ts
@@ -1,11 +1,14 @@
-import AWS from 'aws-sdk'
+import { S3Client } from '@aws-sdk/client-s3'
 
-const endpoint = new AWS.Endpoint(process.env.FLOW_S3_ENDPOINT)
-
-const s3 = new AWS.S3({
-  endpoint: endpoint,
-  accessKeyId: process.env.FLOW_S3_ACCESS_KEY,
-  secretAccessKey: process.env.FLOW_S3_SECRET_KEY,
+const s3Client = new S3Client({
+  // apiVersion: '2006-03-01',
+  endpoint: { hostname: process.env.FLOW_S3_ENDPOINT, path: '', protocol: 'https' },
+  region: 'zrh1', // This needs to be set, but can be anything really b/c we use custom endpoint. E.g. us-east-1
+  credentials: {
+    accessKeyId: process.env.FLOW_S3_ACCESS_KEY,
+    secretAccessKey: process.env.FLOW_S3_SECRET_KEY,
+  },
+  // forcePathStyle: true,
 })
 
-export default s3
+export { s3Client }
