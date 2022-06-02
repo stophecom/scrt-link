69b7b81d5387293bae36b747e364891f24f20579
kinchiller <kingchiller@gmail.com>
2022-06-02 22:03:07 +0200

    Sort translation keys

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -62,7 +62,7 @@ module.exports = {
   // An array of globs that describe where to look for source files
   // relative to the location of the configuration file
 
-  sort: false,
+  sort: true,
   // Whether or not to sort the catalog. Can also be a [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#parameters)
 
   skipDefaultValues: false,
