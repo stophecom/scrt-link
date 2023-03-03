5fe1b56afde89e250fb403f3c248994d51b60fbd
kinchiller <kingchiller@gmail.com>
2023-03-03 22:16:31 +0100

    Rename certificate creation script

--- prepare-ca-certificate.js
+++ prepare-ca-certificate.js
@@ -4,7 +4,12 @@ const path = require('path')
 require('dotenv').config()
 
 // writeFile function with filename, content and callback function
-fs.writeFile('src/ca-certificate.pem', process.env.SCALEGRID_CA_CERTIFICATE, function (err) {
+
+fs.mkdir('tmp', { recursive: true }, (err) => {
+  if (err) throw err
+  console.log('Certificate folder created.')
+})
+fs.writeFile('tmp/ca-certificate.pem', process.env.SCALEGRID_CA_CERTIFICATE, function (err) {
   if (err) throw err
   console.log('Certificate file was created successfully.')
 })
