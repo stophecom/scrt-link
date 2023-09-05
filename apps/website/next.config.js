6451d9f621eb269a1c55814f97c436afc263187c
kinchiller <kingchiller@gmail.com>
2023-09-05 14:19:58 +0200

    Hotfix CSP

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -16,20 +16,20 @@ const config = {
 
     return config
   },
-  async headers() {
-    return [
-      {
-        source: '/l',
-        headers: [
-          // {
-          //   key: 'Content-Security-Policy',
-          //   value:
-          //     "default-src 'self' 'unsafe-inline'; font-src https://fonts.gstatic.com/; style-src 'self' 'unsafe-inline'; style-src-elem https://fonts.googleapis.com/ 'unsafe-inline'; script-src 'self' 'unsafe-eval'; connect-src data:;",
-          // },
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/l',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value:
+  //             "default-src 'self' 'unsafe-inline'; font-src https://fonts.gstatic.com/; style-src 'self' 'unsafe-inline'; style-src-elem https://fonts.googleapis.com/ 'unsafe-inline'; script-src 'self' 'unsafe-eval'; connect-src data:;",
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   redirects: async () => {
     return [
       {
